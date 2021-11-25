e111e97e675d6534b5ef3222928de091cf37a8ae
tatsuya.s <57025585+tatsuya4649@users.noreply.github.com>
2021-11-25 12:27:28 +0900

    added setup

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ JWT library for [fly web server](https://github.com/tatsuya4649/fly).
 
 ## Hello World
 
-```
+```python
 from fly import Fly
-from fly_jwt import require_jwt
+from fly_jwt import require_jwt, jwt_payload
 
 app = Fly()
 
@@ -17,6 +17,8 @@ app = Fly()
 )
 @app.get("/")
 def hello(request):
+	# How to get a JWT payload
+	payload = jwt_payload(request)
     return "Hello World"
 ```
 
@@ -25,7 +27,7 @@ client send request.
 ```
 GET / HTTP1.1
 .
-.
+. 
 .
 Authorization: Bearer `JWT CONTENT`
 ```
---
+++ tests/test_asymmetric.py
@@ -0,0 +1,61 @@
+import pytest
+import sys
+import os
+sys.path.append(
+    os.path.abspath("../fly")
+)
+from fly import Fly
+from fly_jwt import require_jwt
+from fly_jwt.jwt import __fly_jwt
+import jwt
+
+@pytest.fixture(scope="function", autouse=False)
+def init_fly():
+    app = Fly()
+    yield app
+
+TEST_CONTENT={
+    "Hello": "World"
+}
+@pytest.fixture(scope="module", autouse=True)
+def asymmetric_encoded_jwt():
+    with open("conf/server.key") as f:
+        key = f.read()
+
+    encoded = jwt.encode(
+        TEST_CONTENT,
+        key=key,
+        algorithm="RS256"
+    )
+    print(f"test jwt: {encoded}")
+    yield encoded
+
+@pytest.fixture(scope="module", autouse=True)
+def request_test(asymmetric_encoded_jwt):
+    request = dict()
+    request["header"] = list()
+    request["header"].append({
+        "name": "Authorization",
+        "value": f"Bearer {asymmetric_encoded_jwt}",
+    })
+    print(f"test request: {request}")
+    yield request
+
+def auth(info):
+    return True
+
+def test_decode_asymmetric(request_test, init_fly):
+    @require_jwt(
+        auth_handler=auth,
+        algorithm="RS256",
+        private_key_path="conf/server.key",
+        public_key_path="conf/server.pub",
+    )
+    @init_fly.get("/")
+    def hello(request):
+        return "Hello World"
+
+    res = __fly_jwt(request_test)
+    print(f"response: {res}")
+    assert(res == "Hello World")
+
